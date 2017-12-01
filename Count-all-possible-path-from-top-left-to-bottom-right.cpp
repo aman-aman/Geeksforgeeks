b8dcbee666243e6031d980c3086a855d4b6ed1bb
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-12-01 16:51:47 +0530

    Add files via upload

---
+++ Count-all-possible-path-from-top-left-to-bottom-right.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+//aman kumar jha
+#define ll long long
+ll mod=1000000007;
+ll dp[100][100];
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        for(ll i=0;i<n;i++)
+            dp[i][0]=1;
+
+        for(ll i=0;i<m;i++)
+            dp[0][i]=1;
+
+        for(ll i=1;i<n;i++)
+        {
+            for(ll j=1;j<m;j++)
+            {
+                dp[i][j]=(dp[i-1][j]+dp[i][j-1])%mod;
+            }
+        }
+        ll res=dp[n-1][m-1]%mod;
+        printf("%lld\n",res);
+    }
+}
