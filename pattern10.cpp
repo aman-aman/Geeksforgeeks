53d487a7f24d12e100d3d4e775238584c3722631
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-10 00:34:54 +0530

    Add files via upload

---
+++ pattern10.cpp
@@ -0,0 +1,22 @@
+//aman kumar jha
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, j, k, s,n;
+    cin>>n;
+    int a=1;
+    int b=n*n+1;
+    for(i=n;i>=1;i--)
+    {
+        for(s=0;s<n-i;s++)
+			printf(" ");
+        for(j=0;j<i;j++)
+			printf("%d*", a++);
+		for (k=0;k<i-1;k++)
+			printf("%d*",b++);
+        printf("%d\n", b);
+		b-=2*(i-1);
+    }
+}
+
