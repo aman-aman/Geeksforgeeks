5dac809501ed6b53bb209de4130f02f2882a8823
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-09 23:38:24 +0530

    Add files via upload

---
+++ pattern8.cpp
@@ -0,0 +1,34 @@
+/*
+3
+44
+555
+6666
+555
+44
+3
+*/
+//aman kumar jha
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int k=3,i;
+    for(i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+            cout<<k<<" ";
+        k++;
+        cout<<"\n";
+    }
+    k--;
+    k--;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+            cout<<k<<" ";
+        k--;
+        cout<<"\n";
+    }
+}
