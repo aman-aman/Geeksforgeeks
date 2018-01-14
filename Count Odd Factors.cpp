4aed06d9933aca7b598cb53d3770c1676e76e14f
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2018-01-14 16:59:44 +0530

    Add files via upload

---
+++ Count Odd Factors.cpp
@@ -0,0 +1,17 @@
+//aman kumar jha
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+     ll n,c=0;
+    cin>>n;
+    cout<<(int)pow(n,0.5)<<"\n";
+
+    }
+}
